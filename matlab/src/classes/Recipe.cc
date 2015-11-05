ec72442a52f03f45e3eb4f4a9daa604e021164c2
Banthok <matla782@student.liu.se>
2015-11-05 16:47:46 +0100

    pyttepul m Recipe

--- matlab/src/classes/Recipe.cc
+++ matlab/src/classes/Recipe.cc
@@ -1,1 +1,18 @@
 #include "classes/headers/Recipe.h"
+#include <string>
+#include <vector>
+
+void Recipe::setTime( int minutes )
+{
+  minutesTime_ = minutes;
+}
+
+void Recipe::addComment( const string& comment )
+{
+  comments_.push_back(comment);
+}
+
+void Recipe::setGrade( double grade )
+{
+  grade_ = grade;
+}
--- matlab/src/classes/headers/Recipe.h
+++ matlab/src/classes/headers/Recipe.h
@@ -1,14 +1,67 @@
 #ifndef RECIPE_H
 #define RECIPE_H
-//#include <string>
+#include <string>
+#include <vector>
 
-struct MiniRecipie
+
+struct MiniRecipe
 {
-  //string name;  
+  string name_;
+  int minutesTime_;
+  double grade_;
 };
 
-class Recipe{
+class Recipe
+{
+ public:
+  //Konstruktorer
+ Recipe()
+   : name_{}, description_{"Empty"}, minutesTime_{}
+  {}
   
+ Recipe( const string& name, const string& description = "Empty2", int minutes = -1 )
+   : name_{name}, description_{description}, minutesTime_{minutes}
+  {}
+  
+  Recipe( const Recipe& ) = default;
+  Recipe( Recipe&& ) = default;
+
+
+  //Destruktor
+  
+
+
+  //Operatorer
+
+
+
+
+
+  //Medlemsfunktioner
+  void setTime( int );
+  void addComment( const string& );
+  void setGrade( double );
+  //  void addIngredient(s)?
+ 
+
+
+
+
+  //Datamedlemmar
+  string name_;
+  string description_;
+  int minutesTime_;
+
+  vector<string> comments_;
+  double grade_;
+  // vector<RecipeIngredient> ingredients_;
+  // vector<RelatedRecipe> relatedRecipes_;
+  // something something image_;
+
+
+
+ private:
+
 };
 
 #endif
