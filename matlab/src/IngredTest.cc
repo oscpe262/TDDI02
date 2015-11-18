adcfb03ba7ae5752349b38b663e727782db1163f
oscpe262 <oscpe262@student.liu.se>
2015-11-18 12:07:50 +0100

    tbc

--- matlab/src/IngredTest.cc
+++ matlab/src/IngredTest.cc
@@ -5,8 +5,19 @@
 #include <vector>
 #include <algorithm>
 
+void IngCreateTest (const Ingredient& foo ) {
+  std::cout << "Ingredient " << foo.name_ << " added:" << endl 
+	    << "Price: " << foo.price_ << endl
+	    << "Kcal: " << foo.kcal_ << endl;
+  return;
+};
+
+// Ing, RecIng, InfoIng
 int main() {
   // Pröva konstruktorer
+  std::cout << "Initierar \"Mjöl\"" << endl;
+  Ingredient Mjol = {"Mjöl",10,100};
+  IngCreateTest (Mjol);
   // Pröva destruktorer
   // Pröva medlemsfunktioner
   // Pröva åtkomst av medlemsvariabler
