00ba03f4bbef0b163d7eef2eb303e72cec82ed57
Banthok <matla782@student.liu.se>
2015-11-19 15:39:55 +0100

    Shell stuff

--- matlab/src/classes/Shell.cc
+++ matlab/src/classes/Shell.cc
@@ -3,20 +3,43 @@
 #include "classes/headers/Ingredient.h"
 #include "classes/headers/RecipeIngredient.h"
 #include <string>
+#include <vector>
 #include <fstream>
 
 using namespace std;
 
+/*
+ * exportTxt
+ * *importTxt
+ * *exportXml
+ * *importXml
+ * *setScaling
+ *** [SDB] ***
+ * exactMatch
+ * getRecipeResults
+ * getIngredientList
+ * *openRecipe
+ * *openIngredient
+ *** [EDB] ***
+ * *addRecipe
+ * *removeReciope
+ * *addIngredient
+ * *removeIngredient
+ */
+
+
 void Shell::exportTxt( string& fileName = "null" )
 {
   if( fileName == "null" )
-    fileName = currentRecipe_.getName() + ".txt";
+    fileName = currentRecipe_.getName() + ".txt"; // fixa så att .txt-ändelsen är garanterad!!!
+
+  // lägg till test för att filen inte redan existerar!!!
 
   ofstream recipeTxt {fileName};
   if( !recipeTxt )
     {
       recipeTxt.close();
-      // kasta undantag
+      // kasta undantag!!!
     }
       
   recipeTxt << "# " << currentRecipe_.getName() << "\n"
@@ -37,3 +60,30 @@ void Shell::exportTxt( string& fileName = "null" )
 
   recipeTxt.close();
 }
+
+/*************
+ *** [SDB] ***
+ ************/
+
+vector<MiniRecipe>& Shell::exactMatch( const string& name )
+{
+  currentRecipe_ = sDB_.receptmatchning(name);
+  vector<MiniRecipe> vmr { MiniRecipe( currentRecipe_.getName(), currentRecipe_.getMinutesTime(), currentRecipe_.getGrade() ) };
+  return vmr;
+}
+
+vector<MiniRecipe> Shell::getRecipeResults( SearchTerm& searchTerms )
+{
+  recipeSearchResults_ = sDB_.receptsökning(searchTerms); // Ersätt receptsökning med verkliga namnet!!!
+  return recipeSearchResults_;
+} 
+
+vector<string> Shell::getIngredientList()
+{
+  ingredientFullList = sDB_.ingredienslistsökning(); // Ersätt ingredienslistsökning med verkliga namnet!!!
+      return ingredientFullList_;
+}
+
+/*************
+ *** [EDB] ***
+ ************/
--- matlab/src/classes/headers/Shell.h
+++ matlab/src/classes/headers/Shell.h
@@ -4,6 +4,10 @@
 #include <string>
 #include "Recipe.h"
 #include "Ingredient.h"
+#include "SearchTerm.h"
+#include "DB.h"
+#include "EditDB.h"
+#include "SearchDB.h"
 
 using namespace std;
 
@@ -11,17 +15,37 @@ class Shell
 {
  public:
 
-  void exportTxt( string& );
+  void                exportTxt( string& );
+  void                importTxt( string& );
+  void                exportXml( string& );
+  void                importXml( string& );
 
+  void                setScaling( double scaling ) { scaling_ = scaling; }
+ 
 
+ 
+  // SearchDB-funktioner
+  vector<MiniRecipe>& exactMatch( const string& );
+  vector<MiniRecipe>  getRecipeResults( SearchTerm& söktermer ); // returnera konstreferens?
+  vector<string>      getIngredientList(); // returnera konstreferens?
+  Recipe              openRecipe( const MiniRecipe& ); // eller sträng?
+  Ingredient          openIngredient( const string& );
+
+  // EditDB-funktioner
+  void                addRecipe( const Recipe& ); // tillägg och redigering?
+  void                removeRecipe( const Recipe& );
+  void                addIngredient( const Ingredient& );
+  void                removeIngredient( const string& ); // överlagring för Ingredient?
 
   // private:
 
-  Recipe currentRecipe_;
-  Ingredient currentIngredient_;
-  vector<MiniRecipe> recipeSearchResults_;
-  vector<string> ingredientFullList_;
-  double scaling_;
-  // searchterms_
+  SearchDB            sDB_;
+  EditDB              eDB_;
+  
+  Recipe              currentRecipe_;
+  Ingredient          currentIngredient_;
+  vector<MiniRecipe>  recipeSearchResults_;     // RecipeList
+  vector<string>      ingredientFullList_;      // IngredientList // IngredientList = RecipeIngredient-listan i ett receptet just nu
+  double              scaling_;
 };
 #endif
